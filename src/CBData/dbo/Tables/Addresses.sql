f1184ababe764cb0070686b60a1cc7fbc457f38f
Krzysztof Talar <krzysztoftalar@protonmail.com>
2020-03-07 13:30:38 +0100

    Register DB Tables

---
+++ src/CBData/dbo/Tables/Addresses.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Addresses]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AddressTypeID] INT NOT NULL,
+    [HomeNumber] INT NOT NULL, 
+    [Street] NVARCHAR(50) NOT NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    [Country] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_Addresses_ToAddressesTypes] FOREIGN KEY (AddressTypeID) REFERENCES AddressesTypes(Id)    
+)
---
+++ src/CBData/dbo/Tables/Phones.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Phones]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PhoneNumberTypeID] INT NOT NULL, 
+    [PhoneNumber] NVARCHAR(25) NOT NULL, 
+    CONSTRAINT [FK_Phones_ToPhoneNumbersTypes] FOREIGN KEY (PhoneNumberTypeID) REFERENCES PhoneNumbersTypes(Id),
+)
